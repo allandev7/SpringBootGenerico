9d94396e2f73ddae32e28736aaa713879186dc2c
allandev7 <allansilvapereira10@gmail.com>
2019-07-29 12:23:28 -0300

    PUT , DELETE, GET  para Clientes

---
+++ src/main/java/com/allan/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,55 @@
+package com.allan.cursomc.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.allan.cursomc.domain.Cliente;
+
+public class ClienteDTO  implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Length(min = 4, max = 120, message = "O tamanho deve ser entre 4 e 120 caracteres")
+	private String nome;
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Email(message = "Email inválido")
+	private String email;
+	
+	public ClienteDTO() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public ClienteDTO(Cliente obj) {
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.email = obj.getEmail();
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+	
+}
--- src/main/java/com/allan/cursomc/services/ClienteService.java
+++ src/main/java/com/allan/cursomc/services/ClienteService.java
@@ -1,12 +1,20 @@
 package com.allan.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.allan.cursomc.domain.Cliente;
+import com.allan.cursomc.domain.Cliente;
+import com.allan.cursomc.dto.ClienteDTO;
 import com.allan.cursomc.repositories.ClienteRepository;
+import com.allan.cursomc.services.exception.DataIntegrityException;
 import com.allan.cursomc.services.exception.ObjectNotFoundException;
 ;
 
@@ -21,5 +29,44 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado id:" +
 									id+", Tipo: "+ Cliente.class.getName()));
 	} 
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());//verifica se o id existe
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	public void delete (Integer id) {
+		find(id);//verifica se o id existe
+		try {
+			repo.deleteById(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir um cliente que contém"
+					+ " pedidos");
+		}
+		
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage,
+			String orderBy, String direction){
+		
+		
+		PageRequest	pageRequest	= PageRequest.of(page, linesPerPage,
+				Direction.valueOf(direction), orderBy);		
+		return repo.findAll(pageRequest);
+	}	
+	
+	public Cliente fromDTO(ClienteDTO objDTO) {
+		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
 	
 }
