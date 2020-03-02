f04d26db49d4b3e3a17e74213ebdcf3fa9c3a005
Allan Silva <allansilva@MacBook-Pro-de-Allan.local>
2020-03-02 12:43:17 -0300

    subtotal e total do pedido

--- src/main/java/com/allan/cursomc/domain/Pedido.java
+++ src/main/java/com/allan/cursomc/domain/Pedido.java
@@ -61,7 +61,13 @@ public class Pedido implements Serializable{
 	}
 
 
-
+	public double getValorTotal() {
+		double soma = 0.0;
+		for(ItemPedido ip : itens) {
+			soma += ip.getSubTotal();
+		}
+		return soma;
+	}
 	public Integer getId() {
 		return id;
 	}
